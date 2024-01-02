5f8d9955016bf9b89d85e8692fd917da1be392fe
Heloisa <heloisagoncalves26@gmail.com>
2024-01-02 18:52:26 -0300

    Adicionando componentes de redefinição de senha

--- plataforma-softex/src/App.js
+++ plataforma-softex/src/App.js
@@ -1,10 +1,14 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-
+import EnviarEmail from './componentes/lembrar_senha/esqueceu-senha';
+import RedefinirSenha from './componentes/redefinir_senha/redefinindo-senha';
 
 
 function App() {
-  return 
+  return (
+    <RedefinirSenha/>
+    
+  )
 }
 
 export default App;
