f1c8025354475ce229ada5d39600d366a2237418
Deborah Lyra <deborahp.lyra@gmail.com>
2024-01-02 20:02:00 -0300

    ajuste

--- plataforma-softex/src/components/pagina-faltas/faltas.jsx
+++ plataforma-softex/src/components/pagina-faltas/faltas.jsx
@@ -1,16 +1,49 @@
 import Calendario from "../calendario/calendario"
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import './style.css';
 
 function Faltas() {
     return (
         <div className="container">
+        <Calendario />
+        <div className="calendar-info-container">
+            <div className="calendar-info">
+                <div className="dot orange-dot"></div>
+                <p>Dias de aula</p>
+            </div>
+            <div className="calendar-info">
+                <div className="dot green-dot"></div>
+                <p>Presente</p>
+            </div>
+            <div className="calendar-info">
+                <div className="dot red-dot"></div>
+                <p>Ausente</p>
+            </div>
+        </div>
+        <div className="text-container">
+            <p>Em caso de ausência, valorizamos a transparência e a responsabilidade acadêmica.
+                Se você precisar justificar uma falta, utilize este espaço para enviar seu atestado médico.
+                Sua saúde é nossa prioridade, e sua participação é fundamental.
+                Clique abaixo para enviar o atestado e garantir que sua falta seja registrada de maneira adequada.</p>
+            <Button variant="primary" size="lg">Justifique sua falta aqui</Button>{' '}
+        </div>
+
+    </div>
+)
+}
+
+export default Faltas
+
+// codigo sem bootstrap: 
+/*
+  <div className="container">
             <Calendario />
             <div className="calendar-info-container">
                 <div className="calendar-info">
                     <div className="dot orange-dot"></div>
-                    <p>Dias de aulas</p>
+                    <p>Dias de aula</p>
                 </div>
                 <div className="calendar-info">
                     <div className="dot green-dot"></div>
@@ -31,6 +64,4 @@ function Faltas() {
 
         </div>
     )
-}
-
-export default Faltas
+*/
