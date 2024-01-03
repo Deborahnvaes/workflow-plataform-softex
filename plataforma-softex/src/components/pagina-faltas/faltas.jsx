4518289fc51ac9bd8141d82046b73346be203bdc
Deborah Lyra <deborahp.lyra@gmail.com>
2024-01-03 11:32:23 -0300

    responsividade

--- plataforma-softex/src/components/pagina-faltas/faltas.jsx
+++ plataforma-softex/src/components/pagina-faltas/faltas.jsx
@@ -27,7 +27,7 @@ function Faltas() {
                 Se você precisar justificar uma falta, utilize este espaço para enviar seu atestado médico.
                 Sua saúde é nossa prioridade, e sua participação é fundamental.
                 Clique abaixo para enviar o atestado e garantir que sua falta seja registrada de maneira adequada.</p>
-            <Button variant="primary" size="lg">Justifique sua falta aqui</Button>{' '}
+            <Button variant="warning" size="lg">Justifique sua falta aqui</Button>{' '}
         </div>
 
     </div>
