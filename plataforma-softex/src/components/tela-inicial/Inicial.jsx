5ae026214633ddaab41705825c2b2bdfcbeb1195
Déborah Novaes <110925099+Deborahnvaes@users.noreply.github.com>
2024-01-04 15:10:45 -0300

    ajuste de tela inicial

--- plataforma-softex/src/components/tela-inicial/Inicial.jsx
+++ plataforma-softex/src/components/tela-inicial/Inicial.jsx
@@ -1,35 +1,34 @@
 import './style.css'
-import Card from 'react-bootstrap/Card';
+import { Card } from 'react-bootstrap';
 
 function Inicial() {
     return (
-        <section className='card-container'>
+        <>
             <Card style={{ width: '18rem' }} className='card'>
-                <i class="fas fa-id-card card-icon"></i>
+                <i className="fas fa-id-card card-icon"></i>
                 <Card.Body>
                     <Card.Link href="#" className='btn'>Perfil</Card.Link>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }} className='card'>
-                <i class="fas fa-file-upload card-icon"></i>
+                <i className="fas fa-file-upload card-icon"></i>
                 <Card.Body>
                     <Card.Link href="#" className='btn'>Faltas</Card.Link>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }} className='card'>
-                <i class="fas fa-signal card-icon"></i>
+                <i className="fas fa-signal card-icon"></i>
                 <Card.Body>
                     <Card.Link href="#" className='btn'>Ranking</Card.Link>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }} className='card'>
-                <i class="fas fa-comments card-icon"></i>
+                <i className="fas fa-comments card-icon"></i>
                 <Card.Body>
                     <Card.Link href="#" className='btn'>Chat</Card.Link>
                 </Card.Body>
             </Card>
-        </section>
-
+            </>
     );
 }
 
