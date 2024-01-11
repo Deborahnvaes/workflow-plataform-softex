a75c23682f12977eb8cce6e3d1bd9fc333cf8b68
Heloisa <heloisagoncalves26@gmail.com>
2024-01-11 11:34:42 -0300

    Ajustes e responsividade

--- plataforma-softex/src/App.js
+++ plataforma-softex/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import LoginForm from './components/Login/LoginForm.jsx'
-import GlobalStyles from './components/Login/Style/GlobalStyles.jsx';
-
+// import LoginForm from './components/Login/LoginForm.jsx'
+// import GlobalStyles from './components/Login/Style/GlobalStyles.jsx';
+import EnviarEmail from './components/redefinirSenha/lembrar_senha/esqueceu-senha.jsx';
+import RedefinirSenha from './components/redefinirSenha/redefinir_senha/redefinindo-senha.jsx';
 function App() {
   return (
     <>
-    <GlobalStyles />
-    <LoginForm />
+    {/* <GlobalStyles />
+    <LoginForm /> */}
+    <EnviarEmail/>
+    <RedefinirSenha/>
     </>
 )
 
