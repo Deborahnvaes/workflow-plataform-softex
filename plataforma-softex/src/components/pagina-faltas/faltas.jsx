4cd19139b196cd43fdd9f4b3007b79e7d8b3ab23
Deborah Lyra <deborahp.lyra@gmail.com>
2024-01-12 15:54:58 -0300

    acrescentando link form de justificar as faltas

--- plataforma-softex/src/components/pagina-faltas/faltas.jsx
+++ plataforma-softex/src/components/pagina-faltas/faltas.jsx
@@ -34,7 +34,7 @@ function Faltas() {
                 Se você precisar justificar uma falta, utilize este espaço para enviar seu atestado médico.
                 Sua saúde é nossa prioridade, e sua participação é fundamental.
                 Clique abaixo para enviar o atestado e garantir que sua falta seja registrada de maneira adequada.</p>
-            <Button variant="warning">Justifique sua falta aqui</Button>{' '}
+            <Button variant="warning" className="btn-faltas"><a href="https://forms.gle/HAe8nFncW5DuiASu9" target="_blank">Justifique sua falta aqui</a> </Button>{' '}
         </div>
 
     </section>
